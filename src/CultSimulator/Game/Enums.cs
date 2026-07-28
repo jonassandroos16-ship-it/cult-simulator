f306a6a8731091eb574971af17d04ef255ce2824
jonassandroos16-ship-it <jonassandroos16@gmail.com>
2026-07-28 18:53:14 +0200

    Add Bank building type and bank upgrade IDs

--- src/CultSimulator/Game/Enums.cs
+++ src/CultSimulator/Game/Enums.cs
@@ -1,5 +1,5 @@
 namespace CultSimulator.Game;
 
-public enum BuildingType { Shrine, Cathedral, Monolith, Treasury }
-public enum UpgradeId { Hymnal, Relics, Visions, Ascendance }
+public enum BuildingType { Shrine, Cathedral, Monolith, Treasury, Bank }
+public enum UpgradeId { Hymnal, Relics, Visions, Ascendance, BankVault, OffshoreAccounts, DarkLedger, SoulEndowment }
 public enum ResourceKind { Faith, Gold }
