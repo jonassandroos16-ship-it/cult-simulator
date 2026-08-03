14483d006670f675e68c9620dcf94937c6371845
jonassandroos16-ship-it <jonassandroos16@gmail.com>
2026-08-03 22:02:41 +0200

    Fix build: restore Minion, CovenMember, MapNodeState class definitions in OccultState.cs

--- src/CultSimulator/Game/OccultState.cs
+++ src/CultSimulator/Game/OccultState.cs
@@ -1,18 +1,51 @@
+using System.Text.Json.Serialization;
+
 namespace CultSimulator.Game;
+
+public class Minion
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString("N")[..8];
+    public PromotedRole Role { get; set; }
+    public string TraitId { get; set; } = "";
+    public string Name { get; set; } = "";
+
+    [JsonIgnore]
+    public MinionTraitDef? Trait => OccultData.Trait(TraitId);
+}
+
+public class CovenMember
+{
+    public CouncilRole Role { get; set; }
+    public string Name { get; set; } = "";
+    public string MinionId { get; set; } = "";
+    public PromotedRole OriginalRole { get; set; }
+    public string TraitId { get; set; } = "";
+    [JsonIgnore]
+    public MinionTraitDef? Trait => OccultData.Trait(TraitId);
+}
+
+public class MapNodeState
+{
+    public string NodeId { get; set; } = "";
+    public bool Conquered { get; set; }
+    public NodeStance Stance { get; set; } = NodeStance.Harvest;
+    public double RaidTimer { get; set; }
+}
 
 public class OccultState
 {
-    public int Initiates { get; set; }
-    public int SermonPowerLevel { get; set; }
     public double LifetimeFaith { get; set; }
-    public int GrandSacrificeCount { get; set; }
+    public int SermonPowerLevel { get; set; }
     public double EldritchFavor { get; set; }
-    public List<string> OwnedArtifacts { get; set; } = new();
-    public List<string> SocketedArtifacts { get; set; } = new();
-    public int UnlockedSocketCount { get; set; } = OccultBalance.BaseSockets;
-    public List<TechId> UnlockedTechs { get; set; } = new();
+    public int GrandSacrificeCount { get; set; }
+    public int Initiates { get; set; }
+    [JsonIgnore]
+    public int Acolytes { get => Initiates; set => Initiates = value; }
     public List<Minion> Minions { get; set; } = new();
-    public List<CouncilSeat> HighCouncil { get; set; } = new();
+    public List<CovenMember> HighCouncil { get; set; } = new();
+    public List<TechId> UnlockedTechs { get; set; } = new();
+    public List<string> SocketedArtifacts { get; set; } = new();
+    public List<string> OwnedArtifacts { get; set; } = new();
     public List<MapNodeState> MapNodes { get; set; } = new();
     public Dictionary<MaterialKind, int> Materials { get; set; } = new();
     public double FrenzyTimer { get; set; }
@@ -20,15 +53,37 @@ public class OccultState
     public double DarkVigilTimer { get; set; }
     public double WhisperChoirTimer { get; set; }
     public double CovenBlessingTimer { get; set; }
-    public double ElixirTimer { get; set; }
     public double ElixirTapMult { get; set; } = 1.0;
     public double ElixirFaithMult { get; set; } = 1.0;
     public double ElixirPreachMult { get; set; } = 1.0;
     public double ElixirWarStrengthMult { get; set; } = 1.0;
+    public double ElixirTimer { get; set; }
+    public List<string[]> LeyLines { get; set; } = new();
 
+    [JsonIgnore]
     public bool IsFrenzyActive => FrenzyTimer > 0;
+
+    [JsonIgnore]
     public bool IsMassHysteriaActive => MassHysteriaTimer > 0;
+
+    [JsonIgnore]
     public bool IsDarkVigilActive => DarkVigilTimer > 0;
+
+    [JsonIgnore]
     public bool IsWhisperChoirActive => WhisperChoirTimer > 0;
+
+    [JsonIgnore]
     public bool IsCovenBlessingActive => CovenBlessingTimer > 0;
+
+    [JsonIgnore]
+    public int UnlockedSocketCount
+    {
+        get
+        {
+            int count = OccultBalance.BaseSockets;
+            if (UnlockedTechs.Contains(TechId.SecondSocket)) count++;
+            if (UnlockedTechs.Contains(TechId.ThirdSocket)) count++;
+            return Math.Min(count, OccultBalance.MaxSockets);
+        }
+    }
 }
